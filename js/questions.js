0dd9fe6832b1cf11045fc8de11f80d0f75c42e55
Ramon Maturano <jose.r.maturano@wellsfargo.com>
2014-08-08 12:48:48 -0700

    Adding basic folder structure and making example work

---
+++ js/questions.js
@@ -0,0 +1,14 @@
+var data = {
+	questions: [
+			{
+				id: 01,
+				text: 'Me gustan las revistas de mecánica.',
+				options: ['Verdadero', 'Falso']
+			},
+			{
+				id: 02,
+				text: 'Tengo buen apetito.',
+				options: ['Verdadero', 'Falso']
+			}
+		]
+	};
--- temp.js
+++
@@ -1,6 +0,0 @@
-var questionTemp = '<div>
-						<p id="question-{{id}}">{{text}}</p>
-						<input type="radio" name="{{id}}" value="true" />
-						<input type="radio" name="{{id}}" value="false" />
-					</div>',
-
