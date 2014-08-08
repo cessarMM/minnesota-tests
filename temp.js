3706eaa53f855f7789d7d883f3e2b2c8b7669f99
Ramón Maturano <Ramon@MacBookPro.local>
2014-08-08 00:21:07 -0700

    First commit for minnesota test with two test files

---
+++ temp.js
@@ -0,0 +1,6 @@
+var questionTemp = '<div>
+						<p id="question-{{id}}">{{text}}</p>
+						<input type="radio" name="{{id}}" value="true" />
+						<input type="radio" name="{{id}}" value="false" />
+					</div>',
+
